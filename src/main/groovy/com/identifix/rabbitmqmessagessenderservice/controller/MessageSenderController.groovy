0042e86a278ce1149a0f6020c03428ba79beb7d2
Alvarado <artemio.alvarado@audatex.com>
2021-09-08 12:19:34 -0500

    Added endpoint and service class for the functionality to be applied

--- src/main/groovy/com/identifix/rabbitmqmessagessenderservice/controller/MessageSenderController.groovy
+++ src/main/groovy/com/identifix/rabbitmqmessagessenderservice/controller/MessageSenderController.groovy
@@ -1,12 +1,14 @@
 package com.identifix.rabbitmqmessagessenderservice
 
 import com.identifix.rabbitmqmessagessenderservice.proposal.MessageSender
+import com.identifix.rabbitmqmessagessenderservice.service.UtilityService
 import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Component
 import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -21,11 +23,18 @@ import org.springframework.web.bind.annotation.RestController
 class MessageSenderController {
     @Autowired
     MessageSender messageSender
-
+    @Autowired
+    UtilityService utilityService
 
     @PostMapping("/sender")
     ResponseEntity<String> sendMessages(@RequestParam String exchange, @RequestBody String files) {
         String response = messageSender.processFile(exchange,files)
+        new ResponseEntity(response, HttpStatus.ACCEPTED)
+    }
+
+    @GetMapping("/dateValidation/pages")
+    ResponseEntity<String> validatePagesDate(@RequestParam String manualUid, @RequestParam String limitDate) {
+        String response = utilityService.validateManualPagesDate(manualUid,limitDate)
         new ResponseEntity(response, HttpStatus.ACCEPTED)
     }
 }
---
+++ src/main/groovy/com/identifix/rabbitmqmessagessenderservice/service/UtilityService.groovy
@@ -0,0 +1,12 @@
+package com.identifix.rabbitmqmessagessenderservice.service
+
+import groovy.util.logging.Slf4j
+import org.springframework.stereotype.Component
+
+@Component
+@Slf4j
+class UtilityService {
+    String validateManualPagesDate(String manualUid, String limitDate) {
+        "TEST"
+    }
+}
